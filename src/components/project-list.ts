75f724519cb76ba33d1de7f8ef095af726abe9d5
Dmitriy <dima06444@gmail.com>
2022-09-03 00:10:44 +0300

    namespace and es6 modules

---
+++ src/components/project-list.ts
@@ -0,0 +1,70 @@
+import { DragTarget } from "../models/drag-drop.js";
+import { Project, ProjectStatus } from "../models/project.js";
+import { projectState } from "../state/project-state.js";
+import { Component } from "./base.js";
+import { ProjectItem } from "./project-item.js";
+
+export class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget {
+  assignProjects: Project[];
+
+  constructor(private type: "active" | "finished") {
+    super("project-list", "app", false, `${type}-projects`);
+    this.assignProjects = [];
+    this.element.id = `${this.type}-projects`;
+
+    this.configure();
+    this.renderContent();
+  }
+
+  dragLeaveHandler = (_: DragEvent): void => {
+    const listEl = this.element.querySelector("ul")!;
+    listEl.classList.remove("droppable");
+  };
+
+  dragOverHandler = (event: DragEvent): void => {
+    if (event.dataTransfer && event.dataTransfer.types[0] === "text/plain") {
+      event.preventDefault();
+      const listEl = this.element.querySelector("ul")!;
+      listEl.classList.add("droppable");
+    }
+  };
+
+  dropHandler = (event: DragEvent): void => {
+    const prjId = event.dataTransfer!.getData("text/plain");
+    projectState.moveProject(
+      prjId,
+      this.type === "active" ? ProjectStatus.Active : ProjectStatus.Finished
+    );
+    const listEl = this.element.querySelector("ul")!;
+    listEl.classList.remove("droppable");
+  };
+
+  private renderProjects() {
+    const listEl = document.getElementById(`${this.type}-projects-list`) as HTMLUListElement;
+    listEl.innerHTML = "";
+    for (const prjItem of this.assignProjects) {
+      new ProjectItem(this.element.querySelector("ul")!.id, prjItem);
+    }
+  }
+  configure(): void {
+    this.element.addEventListener("dragover", this.dragOverHandler);
+    this.element.addEventListener("dragleave", this.dragLeaveHandler);
+    this.element.addEventListener("drop", this.dropHandler);
+    projectState.addListener((projects: Project[]) => {
+      const relevantProjects = projects.filter((prj) => {
+        if (this.type === "active") {
+          return prj.status === ProjectStatus.Active;
+        }
+        return prj.status === ProjectStatus.Finished;
+      });
+      this.assignProjects = relevantProjects;
+      this.renderProjects();
+    });
+  }
+
+  renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent = this.type.toUpperCase() + " PROJECTS";
+  }
+}
---
+++ src/util/validation.ts
@@ -0,0 +1,31 @@
+// validation
+
+export interface Validatable {
+  value: string;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+export function validate(validatableInput: Validatable) {
+  let isValid = true;
+  const { value, required, minLength, maxLength, min, max } = validatableInput;
+  if (required) {
+    isValid = isValid && value.trim().length > 0;
+  }
+  if (minLength) {
+    isValid = isValid && value.length >= minLength;
+  }
+  if (maxLength) {
+    isValid = isValid && value.length <= maxLength;
+  }
+  if (min != null) {
+    isValid = isValid && +value >= min;
+  }
+  if (max != null) {
+    isValid = isValid && +value <= max;
+  }
+  return isValid;
+}
